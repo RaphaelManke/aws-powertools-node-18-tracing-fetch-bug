1359b4c87cdf92990909c94be78ba34afc99fd75
Raphael Manke <raphael.manke@codecentric.de>
2023-07-20 11:41:02 +0200

    Initial commit

---
+++ test/aws-powertools-node-18-fetch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsPowertoolsNode18Fetch from '../lib/aws-powertools-node-18-fetch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-powertools-node-18-fetch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsPowertoolsNode18Fetch.AwsPowertoolsNode18FetchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
